f68df0cc341594fae4781c50b0c702c9b4f1e851
amitnyc83 <amit_nyc@live.com>
2020-01-10 10:43:53 -0500

    add react-boostrap link and package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./assets/scss/mdb.scss";
 import './index.css';
 import App from './App';
@@ -9,13 +9,14 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk'
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import index from '../src/Store/Reducers/index'
 
 
 
 
-const store = createStore(index, applyMiddleware(thunk))
+const store = createStore(index, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
