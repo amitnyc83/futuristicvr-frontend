166aa34fb36f8bda9e5c97dee35b724a60b79fe4
amitnyc83 <amit_nyc@live.com>
2020-05-04 20:30:44 -0400

    display Stripe checkout form when buy now button is clicked

--- src/Components/Pricing.js
+++ src/Components/Pricing.js
@@ -1,10 +1,36 @@
 import React from "react";
+import { loadStripe } from '@stripe/stripe-js';
 import { MDBRow, MDBCol, MDBCard, MDBCardBody, MDBBtn } from "mdbreact";
 
+const stripePromise = loadStripe('pk_test_GvrAXKanQvCuxR2tTJ8y6t6300fhwHbYd8');
+
 const Pricing = () => {
+
+  const handleClick = async (event) => {
+    // When the customer clicks on the button, redirect them to Checkout.
+    const stripe = await stripePromise;
+    const { error } = await stripe.redirectToCheckout({
+      items: [
+        // Replace with the ID of your SKU
+        {sku: 'sku_HDeyURjOb5UvCC', quantity: 1}
+      ],
+      successUrl: 'https://example.com/success',
+      cancelUrl: 'https://example.com/cancel',
+    })
+    .then(function (result) {
+      if (result.error) {
+        // If `redirectToCheckout` fails due to a browser or network
+        // error, display the localized error message to your customer.
+        var displayError = document.getElementById('error-message');
+        displayError.textContent = result.error.message;
+      }
+    });
+  };
+
+
   return (
     <section className="text-center my-5">
-      <h2 className="h1-responsive font-weight-bold text-center my-5">
+      <h2 className="h1-responsive grey-text font-weight-bold text-center my-5">
         OUR PRICING PLANS
       </h2>
       <p className="grey-text text-center w-responsive mx-auto mb-5">
@@ -24,7 +50,7 @@ const Pricing = () => {
               <p className="grey-text">
                 2 HOUR STANDARD
               </p>
-              <MDBBtn rounded color="light-blue">
+              <MDBBtn rounded color="light-blue" role="link" onClick={handleClick}>
                 Buy now
               </MDBBtn>
             </MDBCardBody>
